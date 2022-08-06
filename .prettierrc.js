28fc15280d4176b93df46f50d46627dc53acf3ab
swh_95 <tjdngur22@gmail.com>
2022-08-06 10:03:03 +0900

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     trailingComma: "es5",//코드 맨 뒤에 콤마(,)를 붙이겠다. 
+    tabWidth: 2,// 탭 너비 스페이스 4번
     semi: true,//세미콜론을 사용하겠다.
     singleQuote: true,// 외따움표로 사용하겠다.
     arrowParens: 'always',//Arrow function에서 파라메터 부분에 괄호를 항상 붙여주는 규칙
