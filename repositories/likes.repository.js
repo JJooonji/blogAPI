5aabaaf4bf6f3d38dee5092bc9d49e2e69ba4413
swh_95 <tjdngur22@gmail.com>
2022-08-11 20:10:42 +0900

    like 2
    like 2

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -1,10 +1,10 @@
 const { like } = require('../models');
-const { Post } = require('../models');
+const { Posts } = require('../models');
 const { Op } = require('sequelize');
 
 class likeRepository {
   checkedPost = async (postId) => {
-    const checkpostId = await Post.findOne({ where: { postId: postId } });
+    const checkpostId = await Posts.findOne({ where: { postId: postId } });
     return checkpostId;
   };
   checkedliked = async (postId, userId) => {
@@ -34,9 +34,9 @@ class likeRepository {
   };
 
   findAllposts = async (likedpostId) => {
-    Post.hasMany(like, { foreignKey: 'postId' });
-    like.belongsTo(Post, { foreignKey: 'postId' });
-    const Posts = await Post.findAll({
+    Posts.hasMany(like, { foreignKey: 'postId' });
+    like.belongsTo(Posts, { foreignKey: 'postId' });
+    const Posts = await Posts.findAll({
       where: { postId: likedpostId },
       include: [
         {
